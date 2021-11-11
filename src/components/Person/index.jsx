c3f682aecd8d299872f65841c763ba6361b5688e
Robert Bristow <robertbristow3@gmail.com>
2021-11-11 16:49:37 +0000

    add rounds to each team and small layout change to tracker page

--- src/components/Person/index.jsx
+++ src/components/Person/index.jsx
@@ -6,22 +6,22 @@ export function Person({ name, teams }) {
     return (
         <div className='card'>
             <h1>{name}'s team at each stage</h1>
-            <ol>
-                <li>{teams[0]}</li>
-                <li>{teams[1]}</li>
-                <li>{teams[2]}</li>
-                <li>{teams[3]}</li>
-                <li>{teams[4]}</li>
-                <li>{teams[5]}</li>
-                <li>{teams[6]}</li>
-                <li>{teams[7]}</li>
-                <li>{teams[8]}</li>
-                <li>{teams[9]}</li>
-                <li>{teams[10]}</li>
-                <li>{teams[11]}</li>
-                <li>{teams[12]}</li>
-                <li>{teams[13]}</li>
-            </ol>
+            <ul>
+                <li>Extra Preliminary Round: {teams[0]}</li>
+                <li>Preliminary Round: {teams[1]}</li>
+                <li>First Round Qualifying: {teams[2]}</li>
+                <li>Second Round Qualifying: {teams[3]}</li>
+                <li>Third Round Qualifying: {teams[4]}</li>
+                <li>Fourth Round Qualifying: {teams[5]}</li>
+                <li>First Round Proper: {teams[6]}</li>
+                <li>Second Round Proper: {teams[7]}</li>
+                <li>Third Round Proper: {teams[8]}</li>
+                <li>Fourth Round Proper: {teams[9]}</li>
+                <li>Fifth Round Proper: {teams[10]}</li>
+                <li>Quarter-Final: {teams[11]}</li>
+                <li>Semi-Final: {teams[12]}</li>
+                <li>Final: {teams[13]}</li>
+            </ul>
         </div>
     )
 }
--- src/pages/Tracker/index.jsx
+++ src/pages/Tracker/index.jsx
@@ -6,13 +6,13 @@ import { Person } from '../../components';
 
 export function Tracker() {
     return (
-        <>
+        <div id='content'>
             <Map />
             <div id='person-div'>
                 <Person name='Rob' teams={['Quorn', 'Quorn', 'Ilkeston Town', 'Halesowen Town', 'Handsworth', 'Pontefract Colliers', 'FC Halifax Town', 'FC Halifax Town']}/>
                 <Person name='Adelle' teams={['Mousehole AFC', 'Mousehole AFC', 'Plymouth Parkway', 'Gosport Borough', 'Yate Town', 'Yate Town', 'Yate Town', 'Yeovil']}/>
                 <Person name='Pat' teams={['Hadley', 'Hadley', 'Hadley', 'Hadley', 'Enfield Town', 'Chelmsford City', 'Harrow Borough', 'Portsmouth']}/>
             </div>
-        </>
+        </div>
     )
 }
