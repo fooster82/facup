599d3982178aaa34f831b729f4b1d9a6563cf06d
Robert Bristow <robertbristow3@gmail.com>
2022-01-06 16:29:26 +0000

    have the frontend login stuff hoepfully working just need to work through the errors then move onto the backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './style.css';
 
-import { App } from './App'
+import { App } from './App';
+const {"v4": uuidv4} = require('uuid');
 
 ReactDOM.render(
     
