d9a14104e218b6375ae5e5c688930a0fcc86cb50
Robert Bristow <robertbristow3@gmail.com>
2022-04-01 11:23:54 +0100

    hopefully final fix

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 import dj_database_url
 import django_heroku
@@ -18,6 +19,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+SECRET_KEY = os.getenv("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
