23938cf7140fddf18d7afb3dd9cf3051018ca934
Robert Bristow <robertbristow3@gmail.com>
2022-04-01 10:46:49 +0100

    set debug to true to find app error

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
