dce69f099aa38255c66eca3f741202bd8db2f484
Robert Bristow <52455526+fooster82@users.noreply.github.com>
2022-04-12 12:39:42 +0100

    start getting the add team code lined up with the dropdown menu component

--- src/components/DropdownMenu/index.jsx
+++ src/components/DropdownMenu/index.jsx
@@ -3,14 +3,16 @@ import './style.css';
 
 import axios from 'axios';
 
-export  function DropdownMenu(props) {
+export function DropdownMenu(props) {
 
     const round = props.round;
     const name = props.name;
+    const addTeam = props.addTeam
 
     const [ showMenu, setShowMenu ] = useState(false);
+    const [ teamToAdd, setTeamToAdd ] = useState(null);
     const [ teams, setTeams ] = useState([]);
-    const [ matchedTeams, setMatchedTeams ] = useState([]);
+    const [ teamNames, setTeamNames ] = useState([])
 
     const teamsMenu = useRef();
     const dropdownForm = useRef();
@@ -21,14 +23,15 @@ export  function DropdownMenu(props) {
         const fetchTeams = async () => {
             try {
                 let { data } = await axios.get('https://facup.herokuapp.com/api/teams/');
-                data.forEach(t => setTeams(prev => [...prev, t.name]));
+                data.forEach(t => setTeams(prev => [...prev, t]));
+                data.forEach(t => setTeamNames(prev => [...prev, t.name]));                
             } catch (err) {
                 console.warn(err);
             }
         } 
         fetchTeams();        
-    }, [])     
-
+    }, []) 
+    
     // Adds the event listener for the closing functionality of the drop down menu
     useEffect(() => {
         const handleClickOutside = e => {
@@ -40,30 +43,38 @@ export  function DropdownMenu(props) {
     }, []);
 
     // When a user clicks one of the team buttons, add that team to the matching text input field
-    function handleClick(team) {
+     function handleClick(team) {
         let selectedTeam = team;
         dropdownForm.current.value = selectedTeam;
+        
+         setTeamToAdd(matchTeam(selectedTeam)); // Try to match the current search term to a team in the DB        
+
+        addTeam("butts") // Sends the team to add back to the parent component
+
         setShowMenu(false); // Close the menu when a team is selected
     }
 
     // Finds a team that best matches what the user has currently typed in
     function findMatch (searchWord) {
-        if (searchWord != '') {            
-            const teamButtonRefs = teamButtons.current.children; // Stores each of the team buttons in a variable
+        const teamButtonRefs = teamButtons.current.children; // Stores each of the team buttons in a variable
 
-            Array.from(teamButtonRefs).forEach(button => button.disabled = false); // Enables all buttons
+        Array.from(teamButtonRefs).forEach(button => button.disabled = false); // Enables all buttons
 
-            // Checks for any matches between the current search term and the team buttons and then returns those that don't
-            const unmatchedTeamButtons = Array.from(teamButtonRefs).filter(button => {
-                    return !Object.values(button.textContent).join('').toLowerCase().includes(searchWord.toLowerCase());
-            })
-            
-            unmatchedTeamButtons.forEach(button => button.disabled = true); // Hides the teams that don't match what is currently typed
-
-        } else {
-            setMatchedTeams("Nothing has been searched so far");
-        }        
+        // Checks for any matches between the current search term and the team buttons and then returns those that don't
+        const unmatchedTeamButtons = Array.from(teamButtonRefs).filter(button => {
+            return !Object.values(button.textContent).join('').toLowerCase().includes(searchWord.toLowerCase());
+        })
+        
+        unmatchedTeamButtons.forEach(button => button.disabled = true); // Hides the teams that don't match what is currently typed
     }
+
+    // Attempt to match a string with a team name in the DB
+    function matchTeam(possibleTeam) {
+        let matchedTeam = teams.find(team => team.name.toLowerCase() === possibleTeam.toLowerCase()); // Search the DB for the team
+
+        if(matchedTeam != undefined) return matchedTeam; // If there is a match then return it
+        else return null;
+    } 
     
     return (
     <div id='dropdown-menu' ref={teamsMenu}>
@@ -82,10 +93,19 @@ export  function DropdownMenu(props) {
                 ? 
                 (   
                     <div ref={teamButtons}  id='dropdown-list'>
-                        <button onClick={e => handleClick(e.target.textContent)} className='dropdown-item' key='-1'>-------------</button>
-                        {teams.map((name, i) => {
+                        <button 
+                            onClick={e => handleClick(e.target.textContent)} 
+                            className='dropdown-item' 
+                            key='-1'
+                        >-------------</button>
+                        {teamNames.map((name, i) => {
                             return (
-                                <button ref={teamButtons[i]} onClick={e => handleClick(e.target.textContent)} className='dropdown-item' key={i}>{name}</button>
+                                <button 
+                                    ref={teamButtons[i]} 
+                                    onClick={e => handleClick(e.target.textContent)} 
+                                    className='dropdown-item' 
+                                    key={i}
+                                >{name}</button>
                             )
                         })}
                     </div>
@@ -95,8 +115,6 @@ export  function DropdownMenu(props) {
                     null
                 )
         } 
-        <div ref={teamButtons}></div>       
     </div>
     )
 }
-// 
--- src/components/SelectTeams/index.jsx
+++ src/components/SelectTeams/index.jsx
@@ -1,17 +1,87 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
 
+import axios
+ from 'axios';
 import { DropdownMenu } from '../DropdownMenu';
 
-export function SelectTeams() {   
+export function SelectTeams() {  
+    const [ users, setUsers ] = useState([]);
+
+    const [ team1, setTeam1] = useState(null);
+    const [ team2, setTeam2] = useState(null);
+    const [ team3, setTeam3] = useState(null);
+    const [ team4, setTeam4] = useState(null);
+    const [ team5, setTeam5] = useState(null);
+    const [ team6, setTeam6] = useState(null);
+    const [ team7, setTeam7] = useState(null);
+    const [ team8, setTeam8] = useState(null);
+    const [ team9, setTeam9] = useState(null);
+    const [ team10, setTeam10] = useState(null);
+    const [ team11, setTeam11] = useState(null);
+    const [ team12, setTeam12] = useState(null);
+    const [ team13, setTeam13] = useState(null);
+    const [ team14, setTeam14] = useState(null);
+
+    useEffect(() => {
+        const fetchTeams = async () => {
+            try {
+                let userData = await axios.get('https://facup.herokuapp.com/api/users/');
+                
+                setUsers(userData);
+            } catch (err) {
+                console.warn(err);
+            }
+        }
+        fetchTeams();
+    }, []);  
+
+    async function postTeams(e) {
+        e.preventDefault(); // Stops the button just reloading the page
+
+        // Get the current year
+        let date = new Date().getFullYear()
+    
+        // Set the ID of the current user and get their username
+        let userId = JSON.parse(localStorage.getItem("user")).id  
+        let username = users.data.find(user => user.id === userId).username;
+
+        // Post the above details to set the user's teams
+        try {
+            await axios.post('https://facup.herokuapp.com/api/stats/', {
+                    user: username,
+                    year: date,
+                    team1: team1.id,
+                    team2: team2.id,
+                    team3: team3.id,
+                    team4: team4.id,
+                    team5: team5.id,
+                    team6: team6.id,
+                    team7: team7.id,
+                    team8: team8.id,
+                    team9: team9.id,
+                    team10: team10.id,
+                    team11: team11.id,
+                    team12: team12.id,
+                    team13: team13.id,
+                    team14: team14.id,
+                })
+        } catch(err) {
+            console.log(err);
+        }
+    }
+    
+    const setTeam1Callback = (teamToAdd) => setTeam1(teamToAdd);
+    const logTeam = () => console.log(team1);
     
     return (
         <>
             <h1>Enter your teams below!</h1>
+            <button onClick={logTeam}>log</button>
 
-            <form id="add-teams-form" onSubmit={e => {e.preventDefault(); console.log("hello")}} >         
+            <form id="add-teams-form" onSubmit={e => {postTeams(e)}} >         
 
-                <DropdownMenu round={'Extra Preliminary Round'} name={'EPR'}/>
+                <DropdownMenu addTeam={setTeam1Callback} round={'Extra Preliminary Round'} name={'EPR'}/>
                 <DropdownMenu round={'Preliminary Round'} name={'PR'}/>
                 <DropdownMenu round={'First Round Qualifying'} name={'Q1'}/>
                 <DropdownMenu round={'Second Round Qualifying'} name={'Q2'}/>
